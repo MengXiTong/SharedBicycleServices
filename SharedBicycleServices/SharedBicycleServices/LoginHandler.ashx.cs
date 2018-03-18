7ddffe1b6e4f9a062b4c78aa8f32f5e29ed8df25
MengXiTong <1317545539@qq.com>
2018-03-18 12:21:51 +0800

    修改

--- SharedBicycleServices/SharedBicycleServices/LoginHandler.ashx.cs
+++ SharedBicycleServices/SharedBicycleServices/LoginHandler.ashx.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace SharedBicycleServices
 {
@@ -13,8 +15,40 @@ namespace SharedBicycleServices
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
+            try
+            {
+                context.Response.ContentType = "text/plain";
+                SqlConnection con = new SqlConnection("server=localhost;database=SharedBicycle;user id=sa;password=123456");
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                StreamReader sr = new StreamReader(context.Request.InputStream);
+                string data = sr.ReadToEnd().ToLower();
+                //string data1 = context.Request.QueryString["age"];
+                if (context.Request.HttpMethod.ToUpper() == "GET")
+                {
+                    cmd.CommandText = "select * from tblUser";
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    String str = "";
+                    while (dr.Read())
+                    {
+                        for (int i = 0; i < dr.FieldCount; i++)
+                        {
+                            str += dr[i].ToString() + ",";
+                        }
+                    }
+                    dr.Close();
+                    con.Close();
+                    context.Response.Write(str);
+                    //context.Response.Write("参数1"+data);
+                    //context.Response.Write("参数2"+data1);
+                }
+            }
+            catch (Exception error)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("false:" + error.ToString());
+            }
         }
 
         public bool IsReusable
